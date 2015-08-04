33034b535b1ffd27a9337a0f8ece3c2496d7c1e8
Edmund Wright <edmund.wright@gmail.com>
2015-08-04 12:05:54 -0400

    Move SlidingPiece logic to its own file and add Bishop class.

--- piece.rb
+++ piece.rb
@@ -10,20 +10,3 @@ class Piece
     [pos[0] + direction[0], pos[1] + direction[1]]
   end
 end
-
-class SlidingPiece < Piece
-  def moves
-    moves = []
-
-    directions.each do |direction|
-      move = pos
-      while board.on_board?(move) && board.empty?(move)
-        moves << move unless move == pos
-        move = Piece.add_direction(move, direction)
-      end
-    end
-
-    moves
-  end
-
-end
---
+++ sliding_piece.rb
@@ -0,0 +1,34 @@
+require_relative 'piece'
+
+class SlidingPiece < Piece
+  def moves
+    moves = []
+
+    directions.each do |direction|
+      move = pos
+      while board.on_board?(move) && board.empty?(move)
+        moves << move unless move == pos
+        move = Piece.add_direction(move, direction)
+      end
+    end
+
+    moves
+  end
+end
+
+class Bishop < SlidingPiece
+  attr_reader :directions
+
+  def initialize(pos, board)
+    super(pos, board)
+    @directions = [[1,1], [1,-1], [-1,1], [-1,-1]]
+  end
+
+  def to_s
+    " B "
+  end
+
+  def inspect
+    " B "
+  end
+end
