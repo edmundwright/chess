947288727d5f27e697e85ae784c6faad62dcc7a3
Edmund Wright <edmund.wright@gmail.com>
2015-08-04 11:34:04 -0400

    Create SlidingPiece#moves.

--- piece.rb
+++ piece.rb
@@ -5,4 +5,25 @@ class Piece
     @pos = pos
     @board = board
   end
+
+  def self.add_direction(pos, direction)
+    [pos[0] + direction[0], pos[1] + direction[1]]
+  end
+end
+
+class SlidingPiece < Piece
+  def moves
+    moves = []
+
+    directions.each do |direction|
+      move = pos
+      while board.on_board?(move) && board.empty?(move)
+        moves << move unless move == pos
+        move = Piece.add_direction(move, direction)
+      end
+    end
+
+    moves
+  end
+
 end
