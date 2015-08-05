85e2515742897914b2726405e9a55c6ebd696c46
Edmund Wright <edmund.wright@gmail.com>
2015-08-05 11:27:05 -0400

    Add BadInput exception to HumanPlayer, and refactor.

--- human_player.rb
+++ human_player.rb
@@ -12,22 +12,29 @@ class HumanPlayer
       puts "Please use the format a1 b2"
       start_pos, end_pos = parse(gets.chomp)
       board.move(start_pos, end_pos)
-    rescue InvalidMove => e
+    rescue InvalidMove, BadInput => e
       puts e.message
-      retry
-    rescue
-      puts "Try again"
       retry
     end
   end
 
-  def translate(pos_input)
-    col_string, row_string = pos_input.split("")
-
-    [(row_string.to_i - 1), col_string.downcase.ord - "a".ord]
-  end
+  private
 
   def parse(string_input)
     string_input.split.map { |pos| translate(pos) }
   end
+
+  def translate(pos_input)
+    col_string, row_string = pos_input.split("")
+    pos = [(row_string.to_i - 1), col_string.downcase.ord - "a".ord]
+
+    unless pos.all? { |coord| coord.between?(0, board.class::BOARD_SIZE - 1) }
+      raise BadInput.new "Invalid input! Try again."
+    end
+
+    pos
+  end
+end
+
+class BadInput < StandardError
 end
